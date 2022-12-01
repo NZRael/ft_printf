43d38720c4bd00657a85fba1af1b9eaf622f8b8e
NZRael <62652645+NZRael@users.noreply.github.com>
2022-12-01 15:08:09 +0100

    Add files via upload
    all is fix, final commit of ft_printf

--- ft_arguments.c
+++ ft_arguments.c
@@ -6,7 +6,7 @@
 /*   By: sboetti <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 09:35:05 by sboetti           #+#    #+#             */
-/*   Updated: 2022/11/30 12:59:16 by sboetti          ###   ########.fr       */
+/*   Updated: 2022/12/01 15:04:55 by sboetti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,46 +28,24 @@ static void	ft_putu(unsigned int n, int *count)
 		ft_putchar_fd('0' + n, 1, count);
 }
 
-static void	ft_puthex(unsigned int n, char *base, int *count)
+static void	ft_puthex(unsigned long n, char *base, int *count)
 {
 	unsigned int	size;
 
-	size = 16;
+	size = ft_strlen(base);
 	if (n >= size)
 	{
 		ft_puthex(n / size, base, count);
 		ft_puthex(n % size, base, count);
 	}
 	else
-	{
 		ft_putchar_fd(base[n], 1, count);
-		count++;
-	}
 }
 
-static void	ft_putadress(unsigned long long arg, int *count)
+static void	ft_putadress(size_t arg, int *count)
 {
-	unsigned long long	adr;
-	const char			*base;
-	char				ptr[12];
-	int					i;
-
-	adr = arg;
-	base = "0123456789abcdef";
-	i = 11;
-	while ((adr / 16) > 0)
-	{
-		ptr[i] = base[(adr % 16)];
-		adr /= 16;
-		i--;
-	}
-	ptr[i] = base[(adr % 16)];
 	ft_putstr_fd("0x", 1, count);
-	while (i < 12)
-	{
-		ft_putchar_fd(ptr[i], 1, count);
-		i++;
-	}
+	ft_puthex(arg, "0123456789abcdef", count);
 }
 
 void	ft_arguments(char str, va_list arg, int *count)
@@ -85,7 +63,7 @@ void	ft_arguments(char str, va_list arg, int *count)
 			ft_putstr_fd(vaa, 1, count);
 	}
 	else if (str == 'p')
-		ft_putadress(va_arg(arg, unsigned long long), count);
+		ft_putadress(va_arg(arg, size_t), count);
 	else if (str == 'i' || str == 'd')
 		ft_putnbr_fd(va_arg(arg, int), 1, count);
 	else if (str == 'u')
